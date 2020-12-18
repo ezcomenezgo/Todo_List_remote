838023520f61b989f3a35d7227956e1a47593669
Claire <s3310507057@gmail.com>
2020-12-18 22:11:51 +0800

    feat : add update todo function

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/todo/new', (req, res) => {
 })
 
 app.post('/todos', (req, res) => {
-  const name = req.body.name // 從req,body拿出表單裡的name資料
+  const name = req.body.name // 從req.body拿出表單裡的name資料
   return Todo.create({ name }) // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
@@ -48,6 +48,26 @@ app.get('/todos/:id', (req, res) => {
     .lean()
     .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(errors))
 })
 
 app.listen(port, () => {

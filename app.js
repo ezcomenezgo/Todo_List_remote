3098b2248571744e4c6383addb74bc97412792fb
Claire <s3310507057@gmail.com>
2020-12-18 20:54:44 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo') //載入Todo model
+const bodyParser = require('body-parser') // 載入body-parser
 const app = express()
 const port = 3000
 
@@ -20,11 +21,25 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 用app.use規定每一筆請求都需要透過body-parser進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // 取出Todo model裡的所有資料
     .lean() // 把Mongoose的Model物件轉換成乾淨的Javascript資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給index樣板
     .catch(error => console.log(error)) //錯誤處理
+})
+
+app.get('/todo/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name // 從req,body拿出表單裡的name資料
+  return Todo.create({ name }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {

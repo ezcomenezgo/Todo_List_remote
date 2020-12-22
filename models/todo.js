e6a30b60c1cbde626decfa63e3a81df5c6cef753
Claire <s3310507057@gmail.com>
2020-12-22 19:20:30 +0800

    feat: model todo add isDone params

--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,10 @@ const todoSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
     required: true // 這個是必填欄位
-  }
+  },
+  isDone: {
+    type: Boolean,
+    default: false, // 預設完成狀態為false
+  },
 })
 module.exports = mongoose.model('Todo', todoSchema)
